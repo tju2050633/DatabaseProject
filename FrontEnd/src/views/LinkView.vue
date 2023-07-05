f9da02f3b8f8800edb37eca86ad3b2d1547772b2
rmEleven <1569323975@qq.com>
2023-07-05 16:33:44 +0800

    完成demo网页

---
+++ FrontEnd/src/views/LinkView.vue
@@ -0,0 +1,38 @@
+<template>
+    <ContentBase>
+        <div class="row">
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="text-center">
+                            <img src="../assets/logo.png" class="rounded" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-9">
+                <div class="card">
+                    <div class="card-body">
+                        <ValidationExample />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </ContentBase>
+</template>
+  
+<script>
+import ContentBase from '../components/ContentBase.vue';
+import ValidationExample from '../components/ValidationExample.vue';
+
+export default {
+    name: 'LinkView',
+    components: {
+        ContentBase,
+        ValidationExample
+    }
+}
+</script>
+  
+<style scoped></style>
