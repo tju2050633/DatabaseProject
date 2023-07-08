a28efcddb5a35f2c0f3abe7c2a8d6f8acdff4ad4
rmEleven <1569323975@qq.com>
2023-07-08 17:08:11 +0800

    初始化注册页面

---
+++ FrontEnd/src/views/RegisterView.vue
@@ -0,0 +1,25 @@
+<template>
+    <ContentBase>
+        <div class="card">
+            <div class="card-body">
+                <div class="d-flex align-items-center">
+                    <strong>Register Loading...</strong>
+                    <div class="spinner-border ms-auto" role="status" aria-hidden="true"></div>
+                </div>
+            </div>
+        </div>
+    </ContentBase>
+</template>
+  
+<script>
+import ContentBase from '../components/ContentBase.vue';
+
+export default {
+    name: 'RegisterView',
+    components: {
+        ContentBase,
+    }
+}
+</script>
+  
+<style scoped></style>
