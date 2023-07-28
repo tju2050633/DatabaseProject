7a5c7e39fd257d282873b5a1de2bee9fffe2b0ee
yyh2154064 <yyh_@tongji.edu.cn>
2023-07-29 01:42:22 +0800

    实现了对garden表的插入删除操作â

---
+++ BackEnd/GardenBLL/GardenBLL.cs
@@ -0,0 +1,34 @@
+﻿using Garden.BLL.Interfaces;
+using Garden.DAL;
+using Garden.Models;
+
+namespace Garden.BLL
+{
+    public class GardenBLL : IGardenBLL
+    {
+        GardenDAL gardenDAL = new();
+        public Garden Get(string id)
+        {
+            return gardenDAL.GetGardenById(id, out _);
+        }
+
+        public string Insert(Garden garden)
+        {
+            gardenDAL.GetGardenById(garden.GardenId, out int status);
+            if (status == 1)
+            {
+                return "garden_id已存在。";
+            }
+
+            bool Ins = gardenDAL.Insert(garden);
+            if (Ins)
+            {
+                return "插入成功";
+            }
+            else
+            {
+                return "插入失败";
+            }
+        }
+    }
+}
---
+++ BackEnd/GardenBLL/Interfaces/IGardenBLL.cs
@@ -0,0 +1,11 @@
+﻿using Garden.Models;
+
+namespace Garden.BLL.Interfaces
+{
+    public interface IGardenBLL
+    {
+        public Garden Get(string id);
+
+        public string Insert(Garden garden);
+    }
+}
