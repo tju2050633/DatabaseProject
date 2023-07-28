bac40c1a53a463cfc5174b2ce7c3d71eda078e89
Cryamor <1507007359@qq.com>
2023-07-28 15:01:57 +0800

    修改了部分API名、在gitignore中添加Garden/.vs文件夹

--- BackEnd/Garden/Controllers/AccountController.cs
+++ BackEnd/Garden/Controllers/AccountController.cs
@@ -18,14 +18,13 @@ namespace Garden.Controllers
         // 返回状态：["账号或密码错误","发生错误","登陆成功"]
         // 登录成功时向login_info中插入一条数据
         [HttpGet]
-        public ActionResult<string> Get(string id, string pwd)
+        public ActionResult<string> Login(string id, string pwd)
         {
             return _accountBLL.Login(id, pwd);
         }
 
         // 输入学工号(id)、密码(pwd)、用户名(name)
         // 返回状态：["用户名已存在","此学工号已被使用","发生错误","注册成功"]
-        // 有bug，目前无法正常插入
         [HttpPost]
         public ActionResult<string> Register(string id, string pwd, string name)
         {
