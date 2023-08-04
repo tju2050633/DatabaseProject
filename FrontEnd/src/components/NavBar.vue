86c25818c8b56f69c3dd4b56bd7a91e09f47936e
rmEleven <1569323975@qq.com>
2023-08-04 22:03:19 +0800

    美化'关于我们'页面

--- FrontEnd/src/components/NavBar.vue
+++ FrontEnd/src/components/NavBar.vue
@@ -1,87 +1,43 @@
 <!-- 导航栏组件 -->
 
 <template>
-  <nav
-    class="navbar navbar-expand-lg fixed-top"
-    style="background-color: rgb(211, 228, 220)"
-  >
+  <nav class="navbar navbar-expand-lg fixed-top" style="background-color: rgb(12, 75, 51)">
     <div class="container-fluid">
 
       <!-- logo与title -->
-      <router-link
-        class="navbar-brand"
-        :to="{ name: 'home' }"
-        style="margin-left: 50px"
-      >
-        <img
-          src="../assets/logo.png"
-          alt="Logo"
-          width="30"
-          class="d-inline-block align-text-top"
-        />
-        <span class="navbar-text"> 共享花园 </span>
+      <router-link class="navbar-brand" :to="{ name: 'home' }" style="margin-left: 50px">
+        <img src="../assets/logo.png" alt="Logo" width="30" class="d-inline-block align-text-top" />
+        <span class="navbar-text" style="color: white;"> 共享花园 </span>
       </router-link>
 
       <!-- 搜索框 -->
-      <form
-        class="d-flex"
-        role="search"
-        style="min-width: 60%; margin-left: auto; margin-right: auto"
-        @submit.prevent
-      >
+      <form class="d-flex" role="search" style="min-width: 60%; margin-left: auto; margin-right: auto" @submit.prevent>
         <!-- 输入框 -->
-        <input
-          class="form-control me-2"
-          type="search"
-          placeholder="搜索"
-          aria-label="Search"
-          v-model="state.searchTerm"
-          @input="filterSearchHistory"
-        />
+        <input class="form-control me-2" type="search" placeholder="搜索" aria-label="Search" v-model="state.searchTerm"
+          @input="filterSearchHistory" />
 
         <!-- 搜索按钮 -->
         <button class="btn btn-outline-success" type="submit" @click="search">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-search"
-            viewBox="0 0 16 16"
-          >
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search"
+            viewBox="0 0 16 16">
             <path
-              d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"
-            />
+              d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
           </svg>
         </button>
       </form>
 
       <!-- 搜索历史记录下拉框 -->
       <li class="nav-item dropdown">
-        <a
-          class="nav-link dropdown-toggle"
-          href="#"
-          role="button"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
+        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
           搜索历史
         </a>
         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
           <ul class="list-group">
-            <li
-              class="list-group-item"
-              v-for="searchTerm in state.filteredSearchHistory"
-              :key="searchTerm"
-              @click="selectSearchTerm(searchTerm)"
-            >
+            <li class="list-group-item" v-for="searchTerm in state.filteredSearchHistory" :key="searchTerm"
+              @click="selectSearchTerm(searchTerm)">
               {{ searchTerm }}
             </li>
-            <li
-              class="list-group-item text-danger"
-              v-if="state.searchHistory.length > 0"
-              @click="clearSearchHistory"
-            >
+            <li class="list-group-item text-danger" v-if="state.searchHistory.length > 0" @click="clearSearchHistory">
               清空历史
             </li>
           </ul>
@@ -93,37 +49,24 @@
         <li class="nav-item dropdown">
 
           <!-- 头像 -->
-          <a
-            class="nav-link dropdown-toggle"
-            href="#"
-            role="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="30"
-              height="30"
-              fill="currentColor"
-              class="bi bi-person"
-              viewBox="0 0 16 16"
-            >
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person"
+              viewBox="0 0 16 16">
               <path
-                d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"
-              />
+                d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
             </svg>
           </a>
 
           <!-- 下拉菜单 -->
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
             <li>
-              <router-link class="dropdown-item" v-if="!$store.state.user.is_login" :to="{ name: 'login' }" >
+              <router-link class="dropdown-item" v-if="!$store.state.user.is_login" :to="{ name: 'login' }">
                 登录
               </router-link>
             </li>
 
             <li>
-              <router-link class="dropdown-item" v-if="$store.state.user.is_login" :to="{ name: 'personalInfo' }" >
+              <router-link class="dropdown-item" v-if="$store.state.user.is_login" :to="{ name: 'personalInfo' }">
                 个人主页
               </router-link>
             </li>
@@ -133,7 +76,7 @@
             </li>
 
             <li>
-              <router-link class="dropdown-item" v-if="!$store.state.user.is_login" :to="{ name: 'register' }" >
+              <router-link class="dropdown-item" v-if="!$store.state.user.is_login" :to="{ name: 'register' }">
                 注册
               </router-link>
             </li>
