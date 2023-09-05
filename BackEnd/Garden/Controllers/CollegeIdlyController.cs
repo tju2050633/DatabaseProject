961e227c2e1385fb638fae6cf2c98a8c23b52a84
UltraTempest10 <104293424+UltraTempest10@users.noreply.github.com>
2023-09-05 13:46:31 +0800

    删除路径中的"api/"，统一路径

--- BackEnd/Garden/Controllers/CollegeIdlyController.cs
+++ BackEnd/Garden/Controllers/CollegeIdlyController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Garden.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class CollegeIdlyController : ControllerBase
     {
