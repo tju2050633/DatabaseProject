57e95655b7d8f692217ad08727a5535d098f3ac5
Jialin Lu <1640889387@qq.com>
2023-09-05 23:38:36 +0800

    主页外观

--- FrontEnd/src/views/HomePageView.vue
+++ FrontEnd/src/views/HomePageView.vue
@@ -16,8 +16,8 @@
           <!-- 走马灯的主页图片 -->
           <div class="images-field">
             <el-carousel :interval="4000" type="card" height="250px" indicator-position="outside">
-              <el-carousel-item v-for="item in mainPageDem" :key="item">
-                <img :src="item.src" style="max-width: 120%" />
+              <el-carousel-item v-for="item in mainPageDem" :key="item" @click="this.$router.push({ name: item.target})">
+                <img :src="item.src" style="max-width: 100%" />
               </el-carousel-item>
             </el-carousel>
           </div>
@@ -35,7 +35,7 @@
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" fill="currentColor" class="bi bi-cursor"
                       viewBox="0 0 16 16">
                       <path
-                        d="M14.082 2.182a.5.5 0 0 1 .103.557L8.528 15.467a.5.5 0 0 1-.917-.007L5.57 10.694.803 8.652a.5.5 0 0 1-.006-.916l12.728-5.657a.5.5 0 0 1 .556.103zM2.25 8.184l3.897 1.67a.5.5 0 0 1 .262.263l1.67 3.897L12.743 3.52 2.25 8.184z" />
+                      d="M8 0a2 2 0 0 0-2 2c0 .552.224 1.052.586 1.414l-.002.002 2.828 2.828a2 2 0 1 0 2.828-2.828l2.828-2.828.002-.002A2 2 0 0 0 14 2a2 2 0 0 0-2-2zm0 3a1 1 0 1 1 0-2 1 1 0 0 1 0 2zm-2.586 2.414a1 1 0 0 1 0-1.414L2.172 2.172a1 1 0 1 1 1.414-1.414l2.828 2.828a1 1 0 0 1 0 1.414l-2.828 2.828a1 1 0 1 1-1.414-1.414l1.414-1.414zm5.172 0a1 1 0 0 1 0-1.414l1.414-1.414a1 1 0 1 1 1.414 1.414l-1.414 1.414a1 1 0 0 1-1.414 0z" />
                     </svg>
                     <el-text>花园展厅</el-text>
                   </div>
@@ -65,8 +65,7 @@
                   <div class="navigator-card">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" fill="currentColor" class="bi bi-cursor"
                       viewBox="0 0 16 16">
-                      <path
-                        d="M14.082 2.182a.5.5 0 0 1 .103.557L8.528 15.467a.5.5 0 0 1-.917-.007L5.57 10.694.803 8.652a.5.5 0 0 1-.006-.916l12.728-5.657a.5.5 0 0 1 .556.103zM2.25 8.184l3.897 1.67a.5.5 0 0 1 .262.263l1.67 3.897L12.743 3.52 2.25 8.184z" />
+                      <path d="M14 0H2C.9 0 0 .9 0 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V2c0-1.1-.9-2-2-2zM3 12v-1h8v1H3zm0-3v-1h8v1H3zm0-3V5h8v1H3zm9 6H2V2h12v10z"/>
                     </svg>
                     <el-text>博客论坛</el-text>
                   </div>
@@ -80,9 +79,7 @@
                   <div class="navigator-card">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" fill="currentColor" class="bi bi-cursor"
                       viewBox="0 0 16 16">
-                      <path
-                        d="M14.082 2.182a.5.5 0 0 1 .103.557L8.528 15.467a.5.5 0 0 1-.917-.007L5.57 10.694.803 8.652a.5.5 0 0 1-.006-.916l12.728-5.657a.5.5 0 0 1 .556.103zM2.25 8.184l3.897 1.67a.5.5 0 0 1 .262.263l1.67 3.897L12.743 3.52 2.25 8.184z" />
-                    </svg>
+                      <path d="M7.5 2.5a.5.5 0 0 1 .5.5v3.793l1.854-1.853a.5.5 0 0 1 .707.707l-2.5 2.5a.5.5 0 0 1-.707 0l-2.5-2.5a.5.5 0 0 1 .707-.707L7 6.293V2a.5.5 0 0 1 .5-.5zm3.5 0a.5.5 0 0 1 .5.5v6.207l1.854-1.853a.5.5 0 0 1 .707.707l-2.5 2.5a.5.5 0 0 1-.707 0l-2.5-2.5a.5.5 0 0 1 .707-.707L11 9.707V3a.5.5 0 0 1 .5-.5z"/>                    </svg>
                     <el-text>当前热榜</el-text>
                   </div>
 
@@ -101,9 +98,7 @@
                   <div class="navigator-card">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" fill="currentColor" class="bi bi-cursor"
                       viewBox="0 0 16 16">
-                      <path
-                        d="M14.082 2.182a.5.5 0 0 1 .103.557L8.528 15.467a.5.5 0 0 1-.917-.007L5.57 10.694.803 8.652a.5.5 0 0 1-.006-.916l12.728-5.657a.5.5 0 0 1 .556.103zM2.25 8.184l3.897 1.67a.5.5 0 0 1 .262.263l1.67 3.897L12.743 3.52 2.25 8.184z" />
-                    </svg>
+                      <path d="M12 1v14H4V1h8m0-1H4C2.9 0 2 .9 2 2v14c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V2c0-1.1-.9-2-2-2zM6 14H4V6h2v8zm3 0H7V9h2v5zm3 0h-2V4h2v10z"/>                    </svg>
                     <el-text>统计数据</el-text>
                   </div>
 
@@ -117,9 +112,7 @@
                   <div class="navigator-card">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" fill="currentColor" class="bi bi-cursor"
                       viewBox="0 0 16 16">
-                      <path
-                        d="M14.082 2.182a.5.5 0 0 1 .103.557L8.528 15.467a.5.5 0 0 1-.917-.007L5.57 10.694.803 8.652a.5.5 0 0 1-.006-.916l12.728-5.657a.5.5 0 0 1 .556.103zM2.25 8.184l3.897 1.67a.5.5 0 0 1 .262.263l1.67 3.897L12.743 3.52 2.25 8.184z" />
-                    </svg>
+                      <path d="M12.5 0h-9C2.57 0 2 .57 2 1.5v13c0 .93.57 1.5 1.5 1.5h9c.93 0 1.5-.57 1.5-1.5v-13c0-.93-.57-1.5-1.5-1.5zM7 13.5c-1.93 0-3.5-1.57-3.5-3.5S5.07 6.5 7 6.5s3.5 1.57 3.5 3.5-1.57 3.5-3.5 3.5zm4-6.5h-2V5h2v2z"/>                    </svg>
                     <el-text>志愿者招募</el-text>
                   </div>
 
@@ -132,9 +125,7 @@
                   <div class="navigator-card">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" fill="currentColor" class="bi bi-cursor"
                       viewBox="0 0 16 16">
-                      <path
-                        d="M14.082 2.182a.5.5 0 0 1 .103.557L8.528 15.467a.5.5 0 0 1-.917-.007L5.57 10.694.803 8.652a.5.5 0 0 1-.006-.916l12.728-5.657a.5.5 0 0 1 .556.103zM2.25 8.184l3.897 1.67a.5.5 0 0 1 .262.263l1.67 3.897L12.743 3.52 2.25 8.184z" />
-                    </svg>
+                      <path d="M14 2H2c-.55 0-1 .45-1 1v10c0 .55.45 1 1 1h12c.55 0 1-.45 1-1V3c0-.55-.45-1-1-1zM3 4h10v2H3V4zm0 4h10v2H3V8zm0 4h10v2H3v-2z"/>                    </svg>
                     <el-text>花园活动</el-text>
                   </div>
 
@@ -148,9 +139,7 @@
                   <div class="navigator-card">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" fill="currentColor" class="bi bi-cursor"
                       viewBox="0 0 16 16">
-                      <path
-                        d="M14.082 2.182a.5.5 0 0 1 .103.557L8.528 15.467a.5.5 0 0 1-.917-.007L5.57 10.694.803 8.652a.5.5 0 0 1-.006-.916l12.728-5.657a.5.5 0 0 1 .556.103zM2.25 8.184l3.897 1.67a.5.5 0 0 1 .262.263l1.67 3.897L12.743 3.52 2.25 8.184z" />
-                    </svg>
+                      <path d="M14 3H2c-.55 0-1 .45-1 1v1h14V4c0-.55-.45-1-1-1zM3 6h10v2H3V6zm-1 4h12v6c0 .55-.45 1-1 1H2c-.55 0-1-.45-1-1v-6h1zm1 1v4h10v-4H3z"/>                    </svg>
                     <el-text>积分商城</el-text>
                   </div>
 
@@ -266,6 +255,10 @@ body {
 }
 
 .navigator-card {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   text-align: center;
   font-weight: bold;
   font-size: 5vh;
@@ -324,10 +317,9 @@ export default {
   name: "HomePageView",
   setup() {
     const mainPageDem = reactive([
-      { description: "", src: require("../assets/logo.png") },
-      { description: "", src: require("../assets/mainpage.jpeg") },
-      { description: "", src: require("../assets/mainpage.jpeg") },
-      { description: "", src: require("../assets/mainpage.jpeg") },
+      { description: "", src: require("../assets/mainpage1.jpeg"), target:"display" },
+      { description: "", src: require("../assets/mainpage2.jpeg"), target:"BlogView" },
+      { description: "", src: require("../assets/mainpage3.jpeg"), target: "PointMall" },
     ]);
 
     const hotImages = reactive([
